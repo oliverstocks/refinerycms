b2dd2e82335085cc9b0e76221d7242c42581c66a
oliver <oliver@68106.(none)>
2010-05-18 11:15:11 +0000

    move to git

--- vendor/plugins/pages/app/helpers/pages_helper.rb
+++ vendor/plugins/pages/app/helpers/pages_helper.rb
@@ -1,2 +1,17 @@
 module PagesHelper
+  
+  # TODO: comment
+  def get_snipplet(snip)
+    # TODO: I dont need Parts, but on the a title = body
+    snipplet = Page.find(snip, :include => [:parts], :conditions => ["page_parts.title = ?", 'Body'], :limit => 1) 
+    
+	content_tag :h3 do 
+      snipplet[:title]
+	  content_tag :p do 
+        snipplet[:body]
+	  end 
+	end 
+    
+  end
+
 end
