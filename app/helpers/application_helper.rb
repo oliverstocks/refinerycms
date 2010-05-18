e111a81bb3100743245362e897783bd80457510b
oliver <oliver@68106.(none)>
2010-05-18 13:48:49 +0000

    change the location on the helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,20 @@
 
 # You can extend refinery with your own functions here and they will likely not get overriden in an update.
 module ApplicationHelper
-  include Refinery::ApplicationHelper # leave this line in to include all of Refinery's core helper methods.
+ 	include Refinery::ApplicationHelper # leave this line in to include all of Refinery's core helper methods.
+  	# Display a Snipplet in a Template
+	# 
+	# Create a page hidden in the menu
+	# and call it via : get_snipplet('about-me')
+ 	def get_snipplet(snip)
 
+ 		# TODO: Move to Model
+ 		snipplet = Page.find(snip, :include => [:parts], :conditions => ["page_parts.title = ?", 'Body'], :limit => 1) 
+		
+		@side_box = content_tag(:h3, snipplet[:title])
+  	 	@side_box << snipplet[:body]
+
+		# Write in the page
+		#@side_box << "<a href=''>Learn more...</a>"
+	end 
 end
