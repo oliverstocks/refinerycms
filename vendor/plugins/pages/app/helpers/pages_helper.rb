521ee2d6467899277f3997fbb66a907ab157d99a
oliver <oliver@68106.(none)>
2010-05-18 11:49:54 +0000

    Working helper

--- vendor/plugins/pages/app/helpers/pages_helper.rb
+++ vendor/plugins/pages/app/helpers/pages_helper.rb
@@ -1,17 +1,18 @@
 module PagesHelper
   
-  # TODO: comment
-  def get_snipplet(snip)
-    # TODO: I dont need Parts, but on the a title = body
-    snipplet = Page.find(snip, :include => [:parts], :conditions => ["page_parts.title = ?", 'Body'], :limit => 1) 
-    
-	content_tag :h3 do 
-      snipplet[:title]
-	  content_tag :p do 
-        snipplet[:body]
-	  end 
-	end 
-    
-  end
+ 	# Display a Snipplet in a Template
+	# 
+	# Create a page hidden in the menu
+	# and call it via : get_snipplet('about-me')
+ 	def get_snipplet(snip)
 
+ 		# TODO: Move to Model
+ 		snipplet = Page.find(snip, :include => [:parts], :conditions => ["page_parts.title = ?", 'Body'], :limit => 1) 
+		
+		@side_box = content_tag(:h3, snipplet[:title])
+  	 	@side_box << snipplet[:body]
+
+		# Write in the page
+		#@side_box << "<a href=''>Learn more...</a>"
+	end
 end
